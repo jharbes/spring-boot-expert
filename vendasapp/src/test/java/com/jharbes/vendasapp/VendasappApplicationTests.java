54324699f9af0832ed4bcda21e054b5f62c4d152
Jorge Harbes <jharbes@hotmail.com>
2023-02-07 15:40:59 -0300

    inicio projeto vendasapp

---
+++ vendasapp/src/test/java/com/jharbes/vendasapp/VendasappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jharbes.vendasapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
