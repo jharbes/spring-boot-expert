36e92c29336f5898105107aa1a91655ff956e336
Jorge Harbes <jharbes@hotmail.com>
2023-02-10 10:31:43 -0300

    trabalhando com multiplos ambientes e profiles

--- vendasapp/src/main/java/com/jharbes/vendasapp/AppConfiguration.java
+++ vendasapp/src/main/java/com/jharbes/vendasapp/AppConfiguration.java
@@ -1,9 +1,18 @@
 package com.jharbes.vendasapp;
 
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class AppConfiguration {
 	
-	
+	@Bean
+	public CommandLineRunner executar() {
+		return args -> {
+			System.out.println("RODANDO A CONFIGURACAO DE DESENVOLVIMENTO");
+		};
+	}
 }
