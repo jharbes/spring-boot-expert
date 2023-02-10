2c46211abd836837027e39956c8b485f24958a6b
Jorge Harbes <jharbes@hotmail.com>
2023-02-10 13:32:51 -0300

    criando as classes do modelo

---
+++ vendasapp/src/main/java/com/jharbes/vendasapp/entities/Cliente.java
@@ -0,0 +1,43 @@
+package com.jharbes.vendasapp.entities;
+
+import java.util.Objects;
+
+public class Cliente {
+
+	private Integer id;
+	private String nome;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Cliente other = (Cliente) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
---
+++ vendasapp/src/main/java/com/jharbes/vendasapp/entities/Produto.java
@@ -0,0 +1,54 @@
+package com.jharbes.vendasapp.entities;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class Produto {
+
+	private Integer id;
+	private String descricao;
+	private BigDecimal preco;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(descricao, id, preco);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Produto other = (Produto) obj;
+		return Objects.equals(descricao, other.descricao) && Objects.equals(id, other.id)
+				&& Objects.equals(preco, other.preco);
+	}
+
+}
