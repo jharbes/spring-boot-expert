a188363de9e24be6634f336987f20f2e3aba8db5
Jorge Harbes <jharbes@hotmail.com>
2023-02-10 11:15:29 -0300

    Create utilitarios.java

---
+++ utilitarios.java
@@ -0,0 +1,77 @@
+utilitarios java
+
+String.format("%.2f",variavel)  - colocar com duas casas decimais***
+
+Locale.setDefault(Locale.US);
+
+Scanner teclado=new Scanner(System.in);
+
+
+teclado.nextLine();  -  nao pular a necessidade de entrada.
+
+
+-------------------------------
+For each em JAVA:
+
+String[] vetor=new String[10];
+
+for (String x:vetor){
+	System.out.println(x);
+}
+-------------------------------
+
+---------------------------------------------------------------
+LISTAS EM JAVA
+
+• Tamanho da lista: size()
+
+• Obter o elemento de uma posição: get(position)
+
+• Inserir elemento na lista: add(obj), add(int, obj)
+
+• Remover elementos da lista: remove(obj), remove(int), removeIf(Predicate)
+	
+• Encontrar posição de elemento: indexOf(obj), lastIndexOf(obj)
+
+• Filtrar lista com base em predicado:
+List<Integer> result = list.stream().filter(x -> x > 4).collect(Collectors.toList());
+
+• Encontrar primeira ocorrência com base em predicado:
+Integer result = list.stream().filter(x -> x > 4).findFirst().orElse(null);
+
+----------------------------------------------------------------
+
+
+
+----------------------------------------------------------------
+
+MVC - Model View Controller
+
+
+MODEL
+	-ENTITIES
+	-ENUMS
+	-EXCEPTION
+	-SERVICES
+
+
+
+------------------------------------------------------------------------
+
+LENDO ARQUIVO TEXTO
+
+String path = "c:\\temp\\in.txt";
+
+try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+	String line = br.readLine();
+
+	while (line != null) {
+		System.out.println(line);
+		line = br.readLine();
+	}
+} catch (IOException e) {
+	System.out.println("Erro: " + e.getMessage());
+}
+		
+
+--------------------------------------------------------------------------
