45e6ca14cf0ecbbc21e24913af768ddc91d24015
Jorge Harbes <jharbes@hotmail.com>
2023-02-13 11:55:18 -0300

    mapeando a entidade cliente para utilizacao do JPA

--- vendasapp/src/main/java/com/jharbes/vendasapp/entities/Cliente.java
+++ vendasapp/src/main/java/com/jharbes/vendasapp/entities/Cliente.java
@@ -2,11 +2,32 @@ package com.jharbes.vendasapp.entities;
 
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+// @Table so é utilizada caso a tabela tenha nome diferente da classe ou haja algum schema a ser determinado para a tabela
+@Entity
+@Table(name = "cliente")
 public class Cliente {
 
+	// annotation @Id é obrigatoria ter para que funcione o JPA, a annotation
+	// GeneratedValue comanda a forma como sera gerada o ID (auto increment), o
+	// @Column serve de maneira parecida com o @Table para a caso o atributo tenha
+	// nome diferente no banco de dados podemos aqui referenciar isso, nao eh o caso
+	// nesse exemplo mas colocaremos apenas pra registrar a possibilidade
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private Integer id;
+
+	// length limita o numero de caracteres desse atributo no banco de dados
+	@Column(name = "nome", length = 100)
 	private String nome;
-	
+
 	public Cliente() {
 	}
 
@@ -58,5 +79,5 @@ public class Cliente {
 	public String toString() {
 		return "Cliente [id=" + id + ", nome=" + nome + "]";
 	}
-	
+
 }
