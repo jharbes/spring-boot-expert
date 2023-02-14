af4b5bd6ef0b3e8844c783034faa7c7d792126b2
Jorge Harbes <jharbes@hotmail.com>
2023-02-14 10:43:34 -0300

    mapeando a entidade produto

--- vendasapp/src/main/java/com/jharbes/vendasapp/entities/Produto.java
+++ vendasapp/src/main/java/com/jharbes/vendasapp/entities/Produto.java
@@ -3,10 +3,26 @@ package com.jharbes.vendasapp.entities;
 import java.math.BigDecimal;
 import java.util.Objects;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produto")
 public class Produto {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "id")
 	private Integer id;
+
+	@Column(name = "descricao")
 	private String descricao;
+
+	@Column(name = "preco_unitario")
 	private BigDecimal preco;
 
 	public Integer getId() {
