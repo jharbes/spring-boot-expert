6d1a7fff1a74258f03fad6912be60783a79320c1
Jorge Harbes <jharbes@hotmail.com>
2023-02-15 11:56:45 -0300

    mapeando as requisicoes com request mapping

--- vendasapp/src/main/java/com/jharbes/vendasapp/controllers/ClienteController.java
+++ vendasapp/src/main/java/com/jharbes/vendasapp/controllers/ClienteController.java
@@ -2,17 +2,21 @@ package com.jharbes.vendasapp.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.jharbes.vendasapp.entities.Cliente;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-	@RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
+	@RequestMapping(value = { "/api/clientes/hello/{nome}", "/api/hello" }, method = RequestMethod.POST, consumes = {
+			"application/json", "application/xml" }, produces = { "application/json", "application/xml" })
 	@ResponseBody
-	public String helloClientes(@PathVariable("nome") String nomeCliente) {
-		return String.format("Hello %s", nomeCliente);
+	public String helloCliente(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente) {
+		return String.format("Hello %s ", nomeCliente);
 	}
 }
