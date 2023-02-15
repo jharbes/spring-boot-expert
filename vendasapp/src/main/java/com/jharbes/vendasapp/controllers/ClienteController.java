f97859b5c071f9035a6c7861a08823e09377f4b6
Jorge Harbes <jharbes@hotmail.com>
2023-02-15 14:56:15 -0300

    PutMapping - atualizando um cliente

--- vendasapp/src/main/java/com/jharbes/vendasapp/controllers/ClienteController.java
+++ vendasapp/src/main/java/com/jharbes/vendasapp/controllers/ClienteController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -47,12 +48,23 @@ public class ClienteController {
 	public ResponseEntity delete(@PathVariable Integer id) {
 		Optional<Cliente> cliente = clienteRepository.findById(id); // retorna optional porque pode ou nao existir um
 		// cliente com esse id
-		
+
 		if (cliente.isPresent()) {
 			clienteRepository.delete(cliente.get());
 			return ResponseEntity.noContent().build();
 		}
-		
+
 		return ResponseEntity.notFound().build();
+	}
+
+	@PutMapping("/api/clientes/{id}")
+	@ResponseBody
+	public ResponseEntity update(@PathVariable Integer id, @RequestBody Cliente cliente) {
+
+		return clienteRepository.findById(id).map(clienteExistente -> {
+			cliente.setId(clienteExistente.getId());
+			clienteRepository.save(cliente);
+			return ResponseEntity.noContent().build();
+		}).orElseGet(() -> ResponseEntity.notFound().build());
 	}
 }
