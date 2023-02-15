17a8b57ac68560dbbe0f5f60a015436063038382
Jorge Harbes <jharbes@hotmail.com>
2023-02-15 13:39:05 -0300

    requisicao GET com parametros e ResponseEntity

--- vendasapp/src/main/java/com/jharbes/vendasapp/VendasappApplication.java
+++ vendasapp/src/main/java/com/jharbes/vendasapp/VendasappApplication.java
@@ -1,11 +1,25 @@
 package com.jharbes.vendasapp;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.jharbes.vendasapp.entities.Cliente;
+import com.jharbes.vendasapp.repositories.ClienteRepository;
 
 @SpringBootApplication
 public class VendasappApplication {
-
+	
+	@Bean
+    public CommandLineRunner commandLineRunner(@Autowired ClienteRepository clientes){
+        return args -> {
+            Cliente c = new Cliente(null, "Fulano");
+            clientes.save(c);
+        };
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(VendasappApplication.class, args);
 	}
--- vendasapp/src/main/java/com/jharbes/vendasapp/controllers/ClienteController.java
+++ vendasapp/src/main/java/com/jharbes/vendasapp/controllers/ClienteController.java
@@ -1,22 +1,36 @@
 package com.jharbes.vendasapp.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.jharbes.vendasapp.entities.Cliente;
+import com.jharbes.vendasapp.repositories.ClienteRepository;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
 
-	@RequestMapping(value = { "/api/clientes/hello/{nome}", "/api/hello" }, method = RequestMethod.POST, consumes = {
-			"application/json", "application/xml" }, produces = { "application/json", "application/xml" })
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	// o @PathVariable indica que o id é uma variavel da url "/{id}" no caso
+	@GetMapping("/{id}")
 	@ResponseBody
-	public String helloCliente(@PathVariable("nome") String nomeCliente, @RequestBody Cliente cliente) {
-		return String.format("Hello %s ", nomeCliente);
+	public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id) {
+		Optional<Cliente> cliente = clienteRepository.findById(id); // retorna optional porque pode ou nao existir um
+																	// cliente com esse id
+		if (cliente.isPresent()) {
+			return ResponseEntity.ok(cliente.get());
+		}
+
+		return ResponseEntity.notFound().build();
 	}
 }
