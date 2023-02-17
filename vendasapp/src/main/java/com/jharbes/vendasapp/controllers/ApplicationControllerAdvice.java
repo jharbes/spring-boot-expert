b71c35d9589857b152c5bd9d3cbcd2d2679b0f8a
Jorge Harbes <jharbes@hotmail.com>
2023-02-17 15:01:53 -0300

    tratando erros na API com o ControllerAdvice e os ExceptionHandler

---
+++ vendasapp/src/main/java/com/jharbes/vendasapp/controllers/ApplicationControllerAdvice.java
@@ -0,0 +1,20 @@
+package com.jharbes.vendasapp.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.jharbes.vendasapp.exceptions.ApiErrors;
+import com.jharbes.vendasapp.exceptions.RegraNegocioException;
+
+@RestControllerAdvice
+public class ApplicationControllerAdvice {
+
+	@ExceptionHandler(RegraNegocioException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public ApiErrors handleRegraNegocioException(RegraNegocioException ex) {
+		String mensagemErro = ex.getMessage();
+		return new ApiErrors(mensagemErro);
+	}
+}
---
+++ vendasapp/src/main/java/com/jharbes/vendasapp/exceptions/ApiErrors.java
@@ -0,0 +1,17 @@
+package com.jharbes.vendasapp.exceptions;
+
+import java.util.Arrays;
+import java.util.List;
+
+import lombok.Getter;
+
+
+public class ApiErrors {
+	
+	@Getter
+	private List<String> errors;
+	
+	public ApiErrors(String messagemErro) {
+		this.errors=Arrays.asList(messagemErro);
+	}
+}
