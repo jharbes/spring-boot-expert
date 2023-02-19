7efbd1a84b990ef0b5ccb6cc12861c32147fbf64
Jorge Harbes <jharbes@hotmail.com>
2023-02-19 09:35:54 -0300

    Update ApiErrors.java

--- vendasapp/src/main/java/com/jharbes/vendasapp/exceptions/ApiErrors.java
+++ vendasapp/src/main/java/com/jharbes/vendasapp/exceptions/ApiErrors.java
@@ -5,13 +5,12 @@ import java.util.List;
 
 import lombok.Getter;
 
-
 public class ApiErrors {
-	
+
 	@Getter
 	private List<String> errors;
-	
+
 	public ApiErrors(String messagemErro) {
-		this.errors=Arrays.asList(messagemErro);
+		this.errors = Arrays.asList(messagemErro);
 	}
 }
